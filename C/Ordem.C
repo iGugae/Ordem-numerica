6896e4f096af7ef11017cbbd830706aa588c22ee
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2024-04-26 21:48:50 -0300

    Ordem em C

--- C/Ordem.C
+++ C/Ordem.C
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main(void){
     //Variáveis
-    int contador,organizador,ordem[5],troca;
+    int contador, organizador, ordem[5], troca;
 
     //Entrada dos números
     printf("Digite 5 digitos e eu colocarei em ordem numerica! \n");
-    for (contador=0;contador<5;contador++){
-        printf("Digite o %d numero: ",contador+1);
-        scanf("%d",&ordem[contador]);
+    for (contador = 0; contador < 5; contador++){
+        printf("Digite o %d numero: ", contador+1);
+        scanf("%d", &ordem[contador]);
     }
 
     //Entrando no looping
     printf("Analisando...\n");
-    for (organizador=0;organizador<4;organizador++){
-        for (contador=organizador+1;contador<5;contador++){
-            if (ordem[organizador]>ordem[contador]){
+    for (organizador = 0; organizador < 4; organizador++){
+        for (contador = organizador+1; contador < 5; contador++){
+            if (ordem[organizador] > ordem[contador]){
                 troca = ordem[contador];
                 ordem[contador] = ordem[organizador];
                 ordem[organizador] = troca;
@@ -24,7 +24,7 @@ int main(void){
 
     //Resultado
     printf("Os numeros em ordem sao: \n");
-    for (organizador=0;organizador<5;organizador++){
+    for (organizador = 0; organizador < 5; organizador++){
         printf("%d  ",ordem[organizador]);
     }
 }
